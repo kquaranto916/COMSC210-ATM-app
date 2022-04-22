c4c9902464236978b4eb5bdf5bef18577f037b6a
kquaranto916 <70352098+kquaranto916@users.noreply.github.com>
2022-04-21 20:30:29 -0400

    Add files via upload

---
+++ location.kt
@@ -0,0 +1,11 @@
+package com.example.front_end_atm_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class location : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
